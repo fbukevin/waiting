f23259543605dc6cc19c5af3c8e30d7f66a0a865
Veck Hsiao <fbukevin@gmail.com>
2015-10-01 15:04:53 +0800

    Update db.js

--- db.js
+++ db.js
@@ -4,13 +4,14 @@ var uristring =
   process.env.MONGOLAB_URI || // if using mongolab
   process.env_MONGOHQ_URI ||  // if using mongohq
   'mongodb://localhost/express-wait'; 
-var wait = new schema({
+  
+var wait = new schema({		// define schema for wait table
 	user_id : String,
 	content : String,
 	update_at : Date
 });
 
-mongoose.model('wait', wait);
+mongoose.model('wait', wait);	// register model
 mongoose.connect(uristring, function(err, res){
   if(err) { 
     console.log('ERROR connected to: ' + uristring);
