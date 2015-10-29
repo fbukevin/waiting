07a7c92acdf7c9ebc012a76545d57a804be52111
fbukevin <fbukevin@gmail.com>
2015-10-29 15:34:50 +0800

    try assign host 0.0.0.0

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.set('port', process.env.PORT || 3000);
+// app.set('port', process.env.PORT || 3000);
+app.listen(3000, '0.0.0.0');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
